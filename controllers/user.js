cc6e24054f04eec0e29d68b4cdec9d06048d5024
Swati Makhija <swatimakhija@Swatis-MacBook-Air.local>
2021-03-04 11:56:05 -0800

    added more imports to user.js controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,2 +1,10 @@
 //Imports 
-const db = require ('../models')
+require ('dotenv').config();
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const jwt = require ('jsonwebtoken');
+const { JWT_SECRET } = process.env;
+
+
+const db = require ('../models');
+
